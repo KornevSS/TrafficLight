1f3763617d5b04dc0515498daabd21777dc42405
S.Kornev <post.kornev@gmail.com>
2021-03-27 10:03:20 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Сергей Корнев on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
